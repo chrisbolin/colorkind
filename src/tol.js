eb94fb3388fc969a23ab0f7fd0f39e67aca705a1
Chris Bolin <bolin.chris@gmail.com>
2020-05-18 00:12:41 -0600

    new build files

--- src/tol.js
+++ src/tol.js
@@ -1,70 +1,71 @@
-// colors from https://personal.sron.nl/~pault/data/tol_colors.py
+// colors from
+// https://personal.sron.nl/~pault/data/tol_colors.py
+// https://personal.sron.nl/~pault/data/colourschemes.pdf
 
-const qualitative = {
-  bright: [
-    "#4477AA",
-    "#EE6677",
-    "#228833",
-    "#CCBB44",
-    "#66CCEE",
-    "#AA3377",
-    "#BBBBBB",
-  ],
-  highContrast: ["#004488", "#DDAA33", "#BB5566"],
-  vibrant: [
-    "#EE7733",
-    "#0077BB",
-    "#33BBEE",
-    "#EE3377",
-    "#CC3311",
-    "#009988",
-    "#BBBBBB",
-  ],
-  muted: [
-    "#CC6677",
-    "#332288",
-    "#DDCC77",
-    "#117733",
-    "#88CCEE",
-    "#882255",
-    "#44AA99",
-    "#999933",
-    "#AA4499",
-    "#DDDDDD",
-  ],
-  light: [
-    "#77AADD",
-    "#EE8866",
-    "#EEDD88",
-    "#FFAABB",
-    "#99DDFF",
-    "#44BB99",
-    "#BBCC33",
-    "#AAAA00",
-    "#DDDDDD",
-  ],
-  // https://www.r-bloggers.com/the-paul-tol-21-color-salute/
-  legacy: [
-    "#332288",
-    "#117733",
-    "#6699cc",
-    "#ddcc77",
-    "#999933",
-    "#cc6677",
-    "#aa4466",
-    "#882255",
-    "#aa4499",
-    "#44aa99",
-    "#661100",
-    "#88ccee",
-  ],
-};
+function qualitative() {
+  return {
+    // Tol: "my default colour scheme for qualitative data is the bright scheme"
+    bright: [
+      "#4477AA",
+      "#EE6677",
+      "#228833",
+      "#CCBB44",
+      "#66CCEE",
+      "#AA3377",
+      "#BBBBBB",
+    ],
+    highContrast: ["#004488", "#DDAA33", "#BB5566"],
+    vibrant: [
+      "#EE7733",
+      "#0077BB",
+      "#33BBEE",
+      "#EE3377",
+      "#CC3311",
+      "#009988",
+      "#BBBBBB",
+    ],
+    muted: [
+      "#CC6677",
+      "#332288",
+      "#DDCC77",
+      "#117733",
+      "#88CCEE",
+      "#882255",
+      "#44AA99",
+      "#999933",
+      "#AA4499",
+      "#DDDDDD",
+    ],
+    light: [
+      "#77AADD",
+      "#EE8866",
+      "#EEDD88",
+      "#FFAABB",
+      "#99DDFF",
+      "#44BB99",
+      "#BBCC33",
+      "#AAAA00",
+      "#DDDDDD",
+    ],
+    // https://www.r-bloggers.com/the-paul-tol-21-color-salute/
+    legacy: [
+      "#332288",
+      "#117733",
+      "#6699cc",
+      "#ddcc77",
+      "#999933",
+      "#cc6677",
+      "#aa4466",
+      "#882255",
+      "#aa4499",
+      "#44aa99",
+      "#661100",
+      "#88ccee",
+    ],
+  };
+}
 
-Object.keys(qualitative).forEach((key) =>
-  console.log(key, qualitative[key].length)
-);
-
-function discreteRainbow() {
+function discrete() {
   const colors = [
     "#E8ECFB",
     "#D9CCE3",
@@ -190,34 +191,9 @@ function discreteRainbow() {
     ],
   ];
 
-  return colorIndexesByNumber.reduce((accumulator, indexes) => {
-    const collection = indexes.map((index) => colors[index]);
-    const key = indexes.length;
-    return { ...accumulator, [key]: collection };
-  }, {});
+  return colorIndexesByNumber.map((indexes) =>
+    indexes.map((index) => colors[index])
+  );
 }
 
-console.log(discreteRainbow());
-/*
-export plans
-colorkind/2 - highContrast
-colorkind/3 - highContrast (total)
-colorkind/4 - bright
-colorkind/5 - bright
-colorkind/6 - bright
-colorkind/7 - bright (total)
-colorkind/8 - light
-colorkind/9 - light (total)
-colorkind/10 - muted (total)
-colorkind/11 - legacy
-colorkind/12 - legacy (total)
-colorkind/tol-high-contrast
-colorkind/tol-bright
-colorkind/tol-light
-colorkind/tol-muted
-colorkind/tol-legacy
-colorkind/tol-discrete-rainbow/2
-colorkind/tol-discrete-rainbow/3
-colorkind/tol-discrete-rainbow/..
-colorkind/tol-discrete-rainbow/23
-*/
+module.exports = { qualitative, discrete };
