cd636b51304a2093308d8fa0c05ff688d2b3cc3f
Chris Bolin <bolin.chris@gmail.com>
2020-05-18 00:12:56 -0600

    reorg src *

---
+++ dist/2.js
@@ -0,0 +1,4 @@
+module.exports = [
+  "#004488",
+  "#DDAA33"
+];
---
+++ dist/8.js
@@ -0,0 +1,10 @@
+module.exports = [
+  "#77AADD",
+  "#EE8866",
+  "#EEDD88",
+  "#FFAABB",
+  "#99DDFF",
+  "#44BB99",
+  "#BBCC33",
+  "#AAAA00"
+];
